00649c6eed7e34bb17fe5b6fab9e3a4995762037
Filipe <128107143+Filipecorr34@users.noreply.github.com>
2025-03-17 02:37:16 -0300

    Update HomeUSER.js

--- frontend/screens/HomeUSER.js
+++ frontend/screens/HomeUSER.js
@@ -17,26 +17,40 @@ const HomeUSER = ({ navigation }) => {
       <View style={styles.bolaAzulClaro} />
       <View style={styles.bolaAzul} />
       <Text style={styles.headerText}>Iniciar Viagem</Text>
-      <Text style={styles.viagemText}>Minhas viagens</Text>
+      <Text style={styles.viagemText}>Tempo:                00:00 horas</Text>
+      <Text style={styles.distanciaText}>Distância:                 0.7km</Text>
+      <Text style={styles.sonolenciaText}>Sonolência</Text>
+      <Text style={styles.velocidadeText}>Velocidade</Text>
+      <Text style={styles.mediaText}>média </Text>
+      <Text style={styles.geral}>GERAL:</Text>
+      <Text style={styles.comportamentoText}>Comportamento tranquilo</Text>
+
 
       <Image source={require('../assets/cima.png')} style={styles.cima} />
       <Image source={require('../assets/meio1.png')} style={styles.meio1} />
       <Image source={require('../assets/meio2.png')} style={styles.meio2} />
       <Image source={require('../assets/baixo.png')} style={styles.baixo} />
+      <Image source={require('../assets/barra.png')} style={styles.barradenavegacao} />
       <Image source={require('../assets/meio1.png')} style={styles.barradenavegacao} />
       <Image source={require('../assets/icon_conducao.png')} style={styles.icon_cond} />
       <Image source={require('../assets/icon_carro.png')} style={styles.icon_car} />
-      <Image source={require('../assets/icon_ia.png')} style={styles.icon_ia} />
+      <Image source={require('../assets/icon_ia.png')} style={styles.icon_ia} />      
 
-      <Text style={styles.carro}>Histórico</Text>
-      <Text style={styles.alerta}>Alertas</Text>
 
       {/* Botão para Navegação para ConducaoUSER */}
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('ConducaoUSER')}
       >
-        <Text style={styles.buttonText}>Navegação</Text>
+        <Text style={styles.buttonText}>Condução</Text>
+      </TouchableOpacity>
+
+      {/* Botão para Navegação para HistoricoUSER */}
+      <TouchableOpacity
+        style={styles.buttonhistorico}
+        onPress={() => navigation.navigate('HistoricoUSER')}
+      >
+        <Text style={styles.buttonhistoricotext}>Histórico</Text>
       </TouchableOpacity>
 
       {/* Botão para Navegação para IA_USER */}
@@ -44,7 +58,7 @@ const HomeUSER = ({ navigation }) => {
         style={styles.buttonia}
         onPress={() => navigation.navigate('IA_USER')}
       >
-        <Text style={styles.buttoniatext}>Alertas</Text>
+        <Text style={styles.buttoniatext}>IA</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.startButton}>
@@ -90,106 +104,149 @@ const styles = StyleSheet.create({
   },
   cima: {
     position: 'absolute',
-    width: 330,
+    width: 300,
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
     height: 95,
     top: 239,
   },
   meio1: {
-    left: 41,
+    left: 56,
     position: 'absolute',
-    width: 161,
+    width: 145,
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
-    height: 95,
+    height: 90,
     top: 340,
   },
   meio2: {
-    left: 209,
+    left: 211,
     position: 'absolute',
-    width: 162,
+    width: 145,
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
-    height: 95,
+    height: 90,
     top: 340,
   },
   baixo: {
-    left: 41,
+    left: 56,
     position: 'absolute',
-    width: 330,
+    width: 300,
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
     height: 95,
     top: 440,
   },
-  barradenavegacao: {
-    position: 'absolute',
-    left: 0,
-    width: 412,
-    height: 70,
-    top: 724,
-  },
   icon_cond: {
-    left: 40,
+    left: 64,
     position: 'absolute',
     width: 30,
     height: 30,
-    top: 730,
+    top: 729,
   },
   icon_car: {
-    left: 172,
+    left: 181,
     position: 'absolute',
     width: 65,
     height: 44,
     top: 722,
   },
   icon_ia: {
-    left: 330,
+    left: 323,
     position: 'absolute',
-    width: 35,
-    height: 31,
+    width: 30,
+    height: 30,
     top: 730,
   },
   headerText: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
-    top: -129,
+    top: -118,
     color: '#FFF',
   },
   viagemText: {
     position: 'absolute', // Posicionamento absoluto
-    left: 129, // Ajuste horizontal
-    top: 270, // Ajuste vertical
+    left: 80, // Ajuste horizontal
+    top: 254, // Ajuste vertical
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFF',
+    zIndex: 2, // Garante que o texto fique sobre a imagem
+  },
+  distanciaText: {
+    position: 'absolute', // Posicionamento absoluto
+    left: 80, // Ajuste horizontal
+    top: 290, // Ajuste vertical
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFF',
+    zIndex: 2, // Garante que o texto fique sobre a imagem
+  },
+  sonolenciaText: {
+    position: 'absolute', // Posicionamento absoluto
+    left: 76, // Ajuste horizontal
+    top: 370, // Ajuste vertical
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFF',
+    zIndex: 2, // Garante que o texto fique sobre a imagem
+  },
+  velocidadeText: {
+    position: 'absolute', // Posicionamento absoluto
+    left: 239, // Ajuste horizontal
+    top: 359, // Ajuste vertical
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFF',    
+    zIndex: 2, // Garante que o texto fique sobre a imagem
+  },
+  mediaText: {
+    position: 'absolute', // Posicionamento absoluto
+    left: 259, // Ajuste horizontal
+    top: 385, // Ajuste vertical
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFF',    
+    zIndex: 2, // Garante que o texto fique sobre a imagem
+  },
+  geral: {
+    position: 'absolute', // Posicionamento absoluto
+    left: 76, // Ajuste horizontal
+    top: 450, // Ajuste vertical
     fontSize: 20,
     fontWeight: 'bold',
     color: '#FFF',
     zIndex: 2, // Garante que o texto fique sobre a imagem
   },
-  alerta: {
+  comportamentoText: {
     position: 'absolute', // Posicionamento absoluto
-    left: 171, // Ajuste horizontal
-    top: 469, // Ajuste vertical
-    fontSize: 20, // Tamanho da fonte
-    color: '#FFF', // Cor do texto
-    fontWeight: 'bold', // Peso da fonte (negrito)
-  },
-  carro: {
-    left: 0,
-    fontSize: 15,
-    marginBottom: 20,
-    top: 453,
+    left: 76, // Ajuste horizontal
+    top: 480, // Ajuste vertical
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#FFF',
+    zIndex: 2, // Garante que o texto fique sobre a imagem
   },
   button: {
     padding: 10,
     borderRadius: 5,
     marginBottom: 20,
-    top: 398,
-    left: -150,
+    top: 455,
+    left: 10,
   },
   buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonhistorico: {
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    top: 390,
+    left: -125,
+  },
+  buttonhistoricotext: {
     color: '#fff',
     fontSize: 16,
   },
@@ -197,8 +254,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginBottom: 20,
-    top: 335,
-    left: 141,
+    top: 325,
+    left: 133,
   },
   buttoniatext: {
     color: '#fff',
@@ -213,6 +270,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
   },
+barradenavegacao: {
+  position: 'absolute',
+  left: 0,
+  width: 412,
+  height: 70,
+  top: 724,
+},
 });
-
-export default HomeUSER;
+export default HomeUSER;
