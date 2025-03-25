233d3e5ee306c62d0c50ed981355594a5109a549
adry <adryelecosta0000@gmail.com>
2025-03-25 00:00:43 -0300

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -6,13 +6,9 @@ import LoginTela from './screens/LoginTela';
 import HomeADM from './screens/HomeADM';
 import HomeUSER from './screens/HomeUSER';
 import CadastroMotorista from './screens/CadastroMotorista';
-import ConducaoUSER from './screens/ConducaoUSER';
-import HistoricoUSER from './screens/HistoricoUSER';
-
-import CameraFunction from "./screens/CameraFunction";
-import IA_USER from './screens/IA_USER'; 
+import CameraFunction from './screens/CameraFunction';
+import Velocidade from './screens/Velocidade';
 import EditarMotorista from './screens/EditarMotorista';
-import ExcluirMotorista from './screens/ExcluirMotorista';
 import VisualizarMotorista from './screens/VisualizarMotorista';
 
 const Stack = createStackNavigator();
@@ -21,7 +17,7 @@ export default function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const backendUrl = 'http://192.168.1.120:5000/login';
+  const backendUrl = 'http://44.196.219.45:5000/login';
 
   const handleLogin = (credentials, navigation) => {
     setLoading(true);
@@ -43,9 +39,11 @@ export default function App() {
       .then((data) => {
         setLoading(false);
         if (data.isAdmin) {
-          navigation.navigate('HomeADM');
+          // Passa o userId para a tela HomeADM
+          navigation.navigate('HomeADM', { userId: data.user_id });
         } else {
-          navigation.navigate('HomeUSER');
+          // Passa o userId para a tela HomeUSER
+          navigation.navigate('HomeUSER', { userId: data.user_id });
         }
       })
       .catch((error) => {
@@ -68,14 +66,11 @@ export default function App() {
         </Stack.Screen>
         <Stack.Screen name="HomeADM" component={HomeADM} options={{ title: 'Admin' }} />
         <Stack.Screen name="EditarMotorista" component={EditarMotorista} />
-        <Stack.Screen name="ExcluirMotorista" component={ExcluirMotorista} />
         <Stack.Screen name="VisualizarMotorista" component={VisualizarMotorista} />
+        <Stack.Screen name="Velocidade" component={Velocidade} />
         <Stack.Screen name="HomeUSER" component={HomeUSER} options={{ title: 'Usuário' }} />
         <Stack.Screen name="CadastroMotorista" component={CadastroMotorista} options={{ title: 'Cadastro Motorista' }} />
-        <Stack.Screen name="CameraFunction" component={CameraFunction}/>
-        <Stack.Screen name="ConducaoUSER" component={ConducaoUSER} options={{ title: 'Condução' }} />
-        <Stack.Screen name="HistoricoUSER" component={HistoricoUSER} options={{ title: 'Histórico' }} />
-        <Stack.Screen name="IA_USER" component={IA_USER} options={{ title: 'IA' }} />
+        <Stack.Screen name="CameraFunction" component={CameraFunction} />
       </Stack.Navigator>
       {error && <Text style={styles.error}>{error}</Text>}
     </NavigationContainer>
